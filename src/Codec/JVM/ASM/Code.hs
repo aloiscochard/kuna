c73b476643047b8ee9f9dfd293175c4cc0dc0217
Alois Cochard <alois.cochard@gmail.com>
2015-06-25 21:32:29 +0200

    Add support for `Code` stack map frames and attributes.

--- src/Codec/JVM/ASM/Code.hs
+++ src/Codec/JVM/ASM/Code.hs
@@ -8,7 +8,7 @@ import Data.Word (Word8, Word16)
 import qualified Data.ByteString as BS
 
 import Codec.JVM.ASM.Code.CtrlFlow
-import Codec.JVM.Attr (Attr(ACode))
+import Codec.JVM.Attr (Attr(ACode, AStackMapTable), StackMapFrame(..), VerifType(..))
 import Codec.JVM.Cond (Cond)
 import Codec.JVM.Const (Const(..), ConstVal)
 import Codec.JVM.ConstPool (ConstPool)
@@ -22,36 +22,44 @@ import qualified Codec.JVM.Opcode as OP
 
 -- TODO Return `Either` with error if flowVal reach negative value (need to add a flag in Flow)
 toAttrs :: Int -> ConstPool -> Code -> [Attr]
-toAttrs as cp code = [ACode maxStack' maxLocals' $ runCode code cp 0] where
-  maxLocals' = max as $ flMax $ cfLocals cf
-  maxStack' = flMax $ cfStack cf
-  cf = ctrlFlow $ code
+toAttrs as cp code = f $ runCode code cp 0 where
+  f (xs, frames) = [ACode maxStack' maxLocals' xs attrs] where
+      maxLocals' = max as $ flMax $ cfLocals cf
+      maxStack' = flMax $ cfStack cf
+      cf = ctrlFlow $ code
+      attrs = if null frames then [] else [AStackMapTable frames]
 
 data Code = Code
   { consts    :: [Const]
   , ctrlFlow  :: CtrlFlow
-  , runCode   :: ConstPool -> Int -> ByteString }
+  , runCode   :: ConstPool -> Int -> (ByteString, [StackMapFrame]) }
 
 instance Monoid Code where
-  mempty = Code [] mempty (const $ const BS.empty)
+  mempty = Code [] mempty (const $ const mempty)
   mappend c0 c1 = Code cs cf f where
     cs = (consts c0 ++ consts c1)
     cf = mappend (ctrlFlow c0) (ctrlFlow c1)
-    f cp off = xs <> runCode c1 cp (off + BS.length xs) where
+    f cp off = xs <> runCode c1 cp (off + (BS.length $ fst xs)) where
       xs = runCode c0 cp off
 
-mkCode :: [Const] -> CtrlFlow -> (ConstPool -> Int -> ByteString) -> Code
+mkCode :: [Const] -> CtrlFlow -> (ConstPool -> Int -> (ByteString, [StackMapFrame])) -> Code
 mkCode cs cf f = Code cs cf f
 
-mkCode' :: [Const] -> Int -> (ConstPool -> Int -> ByteString) -> Code
-mkCode' cs s f = Code cs cf f where cf = mkCtrlFlow (flowInc s) mempty
+mkCode' :: [Const] -> CtrlFlow -> (ConstPool -> Int -> ByteString) -> Code
+mkCode' cs cf f = mkCode cs cf f' where f' cp i = (f cp i, mempty)
+
+mkCode'' :: [Const] -> Int -> (ConstPool -> Int -> ByteString) -> Code
+mkCode'' cs s f = Code cs cf f' where
+  f' cp i = (f cp i, mempty)
+  cf = mkCtrlFlow (flowInc s) mempty
+
 
 mkCodeStack :: Opcode -> Flow -> Code
-mkCodeStack op' fl = mkCode [] cf $ const $ const (packOpcode op') where
+mkCodeStack op' fl = mkCode [] cf $ const $ const ((packOpcode op'), mempty) where
   cf = mkCtrlFlow fl mempty
 
 mkCodeIdx :: Opcode -> Const -> Code
-mkCodeIdx op' c = mkCode' cs 2 f where
+mkCodeIdx op' c = mkCode'' cs 2 f where
   cs = CP.unpack c
   f cp _ =
     (packOpcode op')                                          <>
@@ -59,7 +67,7 @@ mkCodeIdx op' c = mkCode' cs 2 f where
 
 
 codeBytes :: Int -> ByteString -> Code
-codeBytes s xs = Code [] cf $ const $ const xs where cf = mkCtrlFlow (flowInc s) mempty
+codeBytes s xs = Code [] cf $ const $ const (xs, mempty) where cf = mkCtrlFlow (flowInc s) mempty
 
 packOpcode :: Opcode -> ByteString
 packOpcode op' = BS.pack [opcode op']
@@ -84,7 +92,7 @@ aload_0 :: Code
 aload_0 = mkCodeStack OP.aload_0 $ flowInc 1
 
 invoke :: Opcode -> MethodRef -> Code
-invoke op'  mr@(MethodRef _ _ (MethodDesc _ as)) = mkCode cs (mkCtrlFlow flow mempty) f where
+invoke op'  mr@(MethodRef _ _ (MethodDesc _ as)) = mkCode' cs (mkCtrlFlow flow mempty) f where
   flow = flowDec as
   c = CMethodRef mr
   cs = CP.unpack c
@@ -110,11 +118,18 @@ iif cond ok ko  = mkCode cs (mkCtrlFlow flow mempty) f where
   op' = case cond of
           CD.EQ -> OP.ifeq
           CD.NE -> OP.ifne
-  f cp off = fold [xs, ys, zs] where
-    xs = fold [packOpcode op', packI16 . fromIntegral $ (BS.length ys + off + 1)]
-    ys = runCode ko cp (off + BS.length xs)
-    zs = fold [packOpcode OP.goto, packI16 . fromIntegral $ (BS.length okbs + 3), okbs] where
-      okbs = runCode ok cp (BS.length xs + BS.length ys + 3)
+  f cp off = (fold [xs, fst ko', zs], frames) where
+    okStart = off + koOff + 4
+    xs = fold [packOpcode op', packI16 $ fromIntegral okStart]
+    ko' = runCode ko cp (off + BS.length xs)
+    koOff = BS.length $ fst ko'
+    ok' = runCode ok cp (BS.length xs + koOff + 3)
+    okOff = BS.length $ fst ok'
+    zs = fold [packOpcode OP.goto, packI16 . fromIntegral $ okOff + 3, fst ok']
+    frames = snd ko' ++ [SameFrame $ okStart + 2, SameLocals 1 (VPrim JInt)] ++ snd ok'
+
+ireturn :: Code
+ireturn = op 0 OP.ireturn
 
 vreturn :: Code
 vreturn = op 0 OP.vreturn
--- src/Codec/JVM/Attr.hs
+++ src/Codec/JVM/Attr.hs
@@ -12,44 +12,64 @@ import qualified Data.Text as Text
 import Codec.JVM.Const (Const(CUTF8))
 import Codec.JVM.ConstPool (ConstPool, putIx)
 import Codec.JVM.Internal (putI16, putI32)
+import Codec.JVM.Types (PrimType(..))
+
 
 data Attr
   = ACode
     { maxStack  :: Int
     , maxLocals :: Int
-    , code      :: ByteString }
+    , code      :: ByteString
+    , codeAttrs :: [Attr] }
   | AStackMapTable [StackMapFrame]
 
 instance Show Attr where
   show attr = "A" ++ (Text.unpack $ attrName attr)
 
 attrName :: Attr -> Text
-attrName (ACode _ _ _)        = "Code"
+attrName (ACode _ _ _ _)      = "Code"
 attrName (AStackMapTable _)   = "StackMapTable"
 
 unpackAttr :: Attr -> [Const]
-unpackAttr = return . CUTF8 . attrName
+unpackAttr attr@(ACode _ _ _ xs) = (CUTF8 $ attrName attr):(unpackAttr =<< xs)
+unpackAttr attr = return . CUTF8 . attrName $ attr
 
 putAttr :: ConstPool -> Attr -> Put
 putAttr cp attr = do
   putIx cp $ CUTF8 $ attrName attr
-  let xs = runPut $ putAttrBody attr
+  let xs = runPut $ putAttrBody cp attr
   putI32 . fromIntegral $ LBS.length xs
   putByteString $ LBS.toStrict xs
 
-putAttrBody :: Attr -> Put
-putAttrBody (ACode ms ls xs) = do
+putAttrBody :: ConstPool -> Attr -> Put
+putAttrBody cp (ACode ms ls xs attrs) = do
   putI16 ms
   putI16 ls
   putI32 . fromIntegral $ BS.length xs
   putByteString xs
   putI16 0 -- TODO Exception table
-  putI16 0 -- TODO Attributes
-putAttrBody (AStackMapTable xs) = do
+  putI16 $ length attrs
+  mapM_ (putAttr cp) attrs
+putAttrBody _ (AStackMapTable xs) = do
   putI16 $ length xs
   mapM_ putStackMapFrame xs
 
-data StackMapFrame = Same Int
+-- http://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.7.4
+
+data StackMapFrame
+  = SameFrame Int
+  | SameLocals Int VerifType
+  deriving Show
 
 putStackMapFrame :: StackMapFrame -> Put
-putStackMapFrame (Same i) = putWord8 $ fromIntegral i
+putStackMapFrame (SameFrame i)  = putWord8 $ fromIntegral i
+putStackMapFrame (SameLocals i vt) = do
+  putWord8 $ fromIntegral i + 64
+  putVerifType vt
+
+data VerifType
+  = VPrim PrimType
+  deriving Show
+
+putVerifType :: VerifType -> Put
+putVerifType (VPrim JInt) = putWord8 1
